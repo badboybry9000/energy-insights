5329177c742a064d486fdace2dc1a43804110650
badboybry9000 <badboybry9000@gmail.com>
2021-08-18 14:20:36 -0400

    feat: add CO2 forecast store

--- libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-emission-prognosis-http.service.ts
@@ -1,14 +1,9 @@
 // import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Co2EmissionPrognosisRecord } from './co2-emission-prognosis-record';
 
 const url = 'https://www.energidataservice.dk/proxy/api/datastore_search_sql';
-
-export interface Co2EmissionPrognosisRecord {
-  readonly co2Emission: number;
-  readonly minutes5Utc: string;
-  readonly priceArea: 'DK1' | 'DK2';
-}
 
 @Injectable({
   providedIn: 'root',
